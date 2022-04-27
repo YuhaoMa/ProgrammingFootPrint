ac3382fa23bc397e64bf85d0032c53ae3d06ec48
M.G <yuhan.sun@student.kuleuven.be>
2022-04-27 21:56:57 +0200

    登录可以实现

--- app/src/main/java/com/example/app_footprint/MainActivity.java
+++ app/src/main/java/com/example/app_footprint/MainActivity.java
@@ -5,19 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.app_footprint.module.LoginViewModel;
 import com.example.app_footprint.presenter.ViewModeNoti;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     ViewModeNoti viewModeNoti = new LoginViewModel();
     private Button btnLogin;
     private EditText email;
     private TextView passwd;
+    private RequestQueue requestQueue;
+    private String basicurl = "https://studev.groept.be/api/a21pt105/";
+    private TextView sees;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,21 +39,55 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = (Button)findViewById(R.id.btn_login);
         email = findViewById(R.id.txtName);
         passwd = (TextView)findViewById(R.id.editTextTextPassword);
+        sees = findViewById((R.id.textView3));
     }
 
     public void onBtnLogin_Clicker(View caller){
         String user = email.getText().toString();
         String password = passwd.getText().toString();
+        requestQueue = Volley.newRequestQueue(this);
         //if(user.equals('1') ){
           //  System.out.println("True");
 
         //}
-        if(viewModeNoti.checkUserInfo(user,password)){
-            System.out.println("OK");
-        }
-        else {
-            System.out.println("False");
-        }
+        JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET, basicurl+"login/"+user
+                , null,
+                new Response.Listener<JSONArray>()
+                {
+                    @Override
+                    public void onResponse(JSONArray response)
+                    {
+                        try
+                        {
+                            String responseString = "";
+                            JSONObject curObject = response.getJSONObject( 0 );
+                            responseString = curObject.getString("Password").toString();
+                            if(responseString.equals(password) ){
+                                sees.setText("checked");
+                            }
+                            else {
+                                sees.setText("not correct");
+                            }
+                            //sees.setText(responseString);
+                        }
+                        catch( JSONException e )
+                        {
+                            Log.e( "Database", e.getMessage(), e );
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        sees.setText(error.getLocalizedMessage());
+                    }
+                }
+        );
+
+        requestQueue.add(submitRequest);
     }
+
 
 }
